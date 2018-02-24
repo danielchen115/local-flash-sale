59b0f86d5ba5dcb95e35a68620a1f59772edc4e3
Daniel Chen <daniel.chen115@gmail.com>
2018-02-24 13:34:19 -0500

    Minor styling changes

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,23 +6,23 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">All Products</div>
+                    <div style="font-size: 30px" class="card-header"><b>All Products</b></div>
                     <div class="card-body"> @if (session('status'))
                             <div class="alert alert-success"> {{ session('status') }} </div> @endif
                         <table class="table">
-                            <thead>
+                            <thead style="font-size: 20px">
                             <tr>
                                 <th scope="col">#</th>
                                 <th scope="col">Product Name</th>
                                 <th scope="col">Price</th>
-                                <th scope="col">Actions</th>
+                                <th style="text-align: center;" scope="col">Actions</th>
                             </tr>
                             </thead>
                             <tbody style="font-size: 20px"> @foreach ($products as $product)
-                                <tr>
-                                    <th scope="row">{{$product->id}}</th>
-                                    <td>{{ $product->name }}</td>
-                                    <td>${{ $product->price }} {{ $product->unit }}</td>
+                                <tr >
+                                    <th style="vertical-align: middle;" scope="row">{{$product->id}}</th>
+                                    <td style="vertical-align: middle;">{{ $product->name }}</td>
+                                    <td style="vertical-align: middle;">${{ $product->price }} {{ $product->unit }}</td>
                                     <td><a href="{{ route('sales.create') }}" class="btn btn-info btn-block btn-lg"> Start Sale</a></td>
                                 </tr> @endforeach </tbody>
                         </table>
