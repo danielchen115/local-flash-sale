a90aeae2637a736d62cf8320d6f7b73be416b0a7
Daniel Chen <daniel.chen115@gmail.com>
2018-02-24 02:26:05 -0500

    Add seeder for products

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->insert([
+            [
+                'name' => 'Lean Beef',
+                'price' => 8.99,
+                'unit' => 'per pound',
+            ]
+        ]);
+        DB::table('products')->insert([
+            [
+                'name' => 'Sweet Potato',
+                'price' => 2.50,
+                'unit' => 'per pound',
+            ]
+        ]);
+        DB::table('products')->insert([
+            [
+                'name' => '2% Plain Yogurt',
+                'price' => 1.99,
+                'unit' => '',
+            ]
+        ]);
+        DB::table('products')->insert([
+            [
+                'name' => 'Chicken Breast',
+                'price' => 7.69,
+                'unit' => 'per pound',
+            ]
+        ]);
+        DB::table('products')->insert([
+            [
+                'name' => 'Yukon gold potatoes',
+                'price' => 3.49,
+                'unit' => 'per pound',
+            ]
+        ]);
+        DB::table('products')->insert([
+            [
+                'name' => 'Recycled Scotties Kleenex',
+                'price' => 6.50,
+                'unit' => '',
+            ]
+        ]);
+        DB::table('products')->insert([
+            [
+                'name' => 'Spring Onions',
+                'price' => 0.70,
+                'unit' => 'per pound',
+            ]
+        ]);
+    }
+}
